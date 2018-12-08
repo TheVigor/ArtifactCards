17ffd0dafd38f2f1f3c636d994f43e7743ef3c4d
TheVigor <igor.proskuryakov14@gmail.com>
2018-12-08 20:04:02 +0300

    added some fields for detail info

--- app/src/main/java/com/noble/activity/artifactcards/viewmodels/ArtifactCardDetailBindingAdapters.kt
+++ app/src/main/java/com/noble/activity/artifactcards/viewmodels/ArtifactCardDetailBindingAdapters.kt
@@ -1,7 +1,10 @@
 package com.noble.activity.artifactcards.viewmodels
 
 import android.databinding.BindingAdapter
+import android.os.Build
+import android.text.Html
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.noble.activity.artifactcards.imageloader.ImageLoader
@@ -10,5 +13,16 @@ import com.noble.activity.artifactcards.imageloader.ImageLoader
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         ImageLoader.get().load(view, imageUrl!!)
+    }
+}
+
+@BindingAdapter("textFromHtml")
+fun bindTextFromHtml(view: TextView, htmlText: String?) {
+    if (!htmlText.isNullOrEmpty()) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            view.text = Html.fromHtml(htmlText, Html.FROM_HTML_MODE_COMPACT)
+        } else {
+            view.text = Html.fromHtml(htmlText)
+        }
     }
 }

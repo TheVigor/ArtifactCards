94795112d7eb7653bc3cf7cbfdeda68de45388ad
TheVigor <igor.proskuryakov14@gmail.com>
2019-01-03 19:36:54 +0300

    added logic for update cards database via settings

--- app/src/main/java/com/noble/activity/artifactcards/MainActivity.kt
+++ app/src/main/java/com/noble/activity/artifactcards/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.noble.activity.artifactcards
 
+import android.app.ProgressDialog
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.noble.activity.artifactcards.artifact.main.ArtifactCardViewModel
+import com.noble.activity.artifactcards.utils.HERO_CARD_TYPE
+import com.noble.activity.artifactcards.utils.LoadStatus
 import com.noble.activity.artifactcards.utils.showToast
 
 class MainActivity : AppCompatActivity() {
 
     private var mainFragment: MainFragment? = null
 
+    private lateinit var updateViewModel: ArtifactCardViewModel
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.container)
+
+        updateViewModel = ViewModelProviders.of(this).get(ArtifactCardViewModel::class.java)
+        initLiveData()
 
         if (mainFragment == null) {
             mainFragment = MainFragment.newInstance()
@@ -34,6 +46,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateCards(view: View) {
-        showToast("Clicked")
+        updateViewModel.getAllCards(HERO_CARD_TYPE)
+    }
+
+    private fun initLiveData() {
+        progressDialog = ProgressDialog(this, R.style.DownloadDialog)
+        progressDialog.max = 100
+        progressDialog.setMessage(getString(R.string.loading))
+        progressDialog.setTitle(getString(R.string.downloading_from_api))
+        progressDialog.setCancelable(false)
+
+        updateViewModel.loadStatusLiveData.observe(this@MainActivity,
+            Observer { loadStatus ->
+                loadStatus?.let {
+                    if (LoadStatus.LOADING == loadStatus) {
+                        progressDialog?.show()
+                    } else {
+                        progressDialog?.dismiss()
+                    }
+                }
+            })
     }
 }

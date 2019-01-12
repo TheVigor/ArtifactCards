098c1a9a3e1fd066db09b9546310c474ad1fece1
TheVigor <igor.proskuryakov14@gmail.com>
2019-01-12 16:39:45 +0300

    added more prepopulated decks

--- app/src/main/java/com/noble/activity/artifactcards/prefs/DecksPrefs.kt
+++ app/src/main/java/com/noble/activity/artifactcards/prefs/DecksPrefs.kt
@@ -10,11 +10,18 @@ class DecksPrefs(context: Context) {
             "ADCJSIMJLkCChFLi3hdQqzdAUGFgoqbaAJDBH8BdQFCKgEvAUZhY2UgU21hc2hlcg__",
             "ADCJSYJNrgCWQyTAy29ApIMkkFBQ6EDiEQiAYKTkZSNTW9ubyBCbHVl",
             "ADCJWEQNrgCEY8JT3hdvd8BH0qTIwEDiqoBh4ZZgmQBYQFFY29ub21pYyBDb250cm9s",
-            "ADCJcUQL7kCQYsNCbhdgXfdAUqrAZqfpQNIhI9oAXIBSHlwZWQgQmx1ZS9HcmVlbg__"
+            "ADCJcUQL7kCQYsNCbhdgXfdAUqrAZqfpQNIhI9oAXIBSHlwZWQgQmx1ZS9HcmVlbg__",
+
+            "ADCJVMavrgCygUGzgRZoLsCTopHlKYBZAFBBE1TuQFbrQIWUmVkL0dyZWVuIE1pZHJhbmdlIFN1c3RhaW4_",
+            "ADCJbIIJLkCSpHLBMsFZrsChkKKm0FsAUZVQ1ESSyoCXBBdGlRlbGVwb3J0",
+            "ADCJbUUMbgCXsEFhMgEcLsCi55Bn4wZBQxMCEkBPgGTbAGNVS9HIFJlZ2VuIENyZWVwIFJhbXA_",
+            "ADCJSUPNrgCl84E1gVEuF2g3gGbQUZlAZ8SUaECQXUBRqQBSlJlZCBCbHVlIEJ1ZGdldA__",
+            "ADCJVIg7bkCBQGRywRLprsCgYWMm6gCsQGBVCEBhw2CUmVkL0JsYWNrIC0gcGVyZmVjdCBzdGFydGVyIGRlY2s_"
+
         )
     }
 
-    private val PREFS_FILENAME = "decks_prefs"
+    private val PREFS_FILENAME = "decks_prefs_updated"
     private val CURRENT_DECKS = "current_decks"
 
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, Context.MODE_PRIVATE)
